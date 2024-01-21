3eeabe2e9d23f669358989d22189e2efe123a9e3
argyyyn <argyyyn@gmail.com>
2024-01-21 19:11:52 +0600

    routing

---
+++ app/_components/page.tsx
@@ -0,0 +1,9 @@
+const ComponentPage = () => {
+    return (
+        <div>
+            <h1>Component page</h1>
+        </div>
+    )
+}
+
+export default ComponentPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import {Button} from "@/components/ui/button"
 export default function Home() {
+  console.log('argo')
   return (
       <Button size="lg" variant="outline">Click me</Button>
   )
